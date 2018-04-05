20e4521864956e71fd800032db99cade145af5f6
kerolayne <kerolaynesouza@outlook.com.br>
2018-04-05 17:25:51 -0300

    exercicio1

---
+++ ex-quadrante.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+    float x, y;
+
+    scanf("%f", &x);
+    scanf("%f", &y);
+
+    if(x == 0 && y == 0)
+        printf("Origem\n");
+    else
+        if(x != 0 && y == 0)
+            printf("Eixo X\n");
+        else
+            if(x == 0 && y != 0)
+                printf("Eixo Y\n");
+            else
+                if(x > 0 && y > 0)
+                    printf("Q1\n");
+                else
+
+                  if(x < 0 && y > 0)
+                      printf("Q2\n");
+                  else
+                      if(x < 0 && y < 0)
+                          printf("Q3\n");
+                      else
+                          printf("Q4\n");
+
+    return 0;
+}
