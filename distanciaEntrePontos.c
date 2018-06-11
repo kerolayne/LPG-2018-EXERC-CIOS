cc57027d235481dd764cbaaf6b5e971bfd5610a0
kerolayne <kerolaynesouza@outlook.com.br>
2018-06-11 18:18:45 -0300

    Create distanciaEntrePontos.c

---
+++ distanciaEntrePontos.c
@@ -0,0 +1,6 @@
+/*
+defina uma estrutura para representar um ponto no plano cartesiano (coordenadas x e y -> float).
+escreva um programa que leia dois pontos, mostre na tela, e cacule a distancia entre eles...
+
+d=raiz((a *x -b*x)² + (a *y -b*y)²)
+*/
